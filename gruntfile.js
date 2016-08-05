5e256b56c39eaf2bde86b80fe334e0c3f31eec61
Chris Beard <coebe500@aol.com>
2016-08-05 12:37:06 +0100

    Multiple entry points required and so webpack is a no go for now.

--- gruntfile.js
+++ gruntfile.js
@@ -9,32 +9,43 @@ module.exports = function(grunt) {
     // create link to package.json items
     pkg: grunt.file.readJSON('package.json'),
 
+    // grunt-webpack
+    webpack: {
+      task: {
+        entry: "./components/js/test.js", // CB note that to get more than one file, you'd need to set multiple entry points :(
+        output: {
+          filename: "webpack-compressed.js"
+        }
+      }
+    }
+
     // grunt-contrib-concat. Concat for dev and production
-    concat: {
-        prod: {
-          src: 'components/js/**/*.js',
-          dest: 'builds/production/js/concat.js'
-          },
-        dev: { // is dev needed? leave here until decided
-          src: 'components/js/*',
-          dest: 'builds/development/js/concat.js'
-          }
-    },
+    // concat: {
+    //     prod: {
+    //       src: 'components/js/**/*.js',
+    //       dest: 'builds/production/js/concat.js'
+    //       },
+    //     dev: { // is dev needed? leave here until decided
+    //       src: 'components/js/*',
+    //       dest: 'builds/development/js/concat.js'
+    //       }
+    // },
 
     // grunt-contrib-uglify. Uglify only on production
-    uglify: {
-        prod: {
-          files: {
-            'builds/production/js/concat.min.js': ['builds/production/js/concat.js']
-          }
-        }
-    }
+    // uglify: {
+    //     prod: {
+    //       files: {
+    //         'builds/production/js/concat.min.js': ['builds/production/js/concat.js']
+    //       }
+    //     }
+    // }
 
   }); //initConfig
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-webpack');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
-  
+  // grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['webpack']);  
 }; // wrapper function
